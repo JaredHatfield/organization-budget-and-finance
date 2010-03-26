fea961f4faadff9b55e354c7abcae48f2590de27
jjhatf02 <jjhatf02@90e9d07a-2c5a-11df-a14a-e9fb886a093d>
2010-03-26 22:35:10 +0000

    Merged changes from the development_search branch into the trunk.

---
+++ trunk/common/include.search.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * Project:     organization-budget-and-finance
+ * File:        include.search.php
+ *
+ * organization-budget-and-finance is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public License 
+ * as published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ * 
+ * organization-budget-and-finance is distributed in the hope that it 
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with organization-budget-and-finance.  If not, see 
+ * http://www.gnu.org/licenses/.
+ *
+ * @link http://code.google.com/p/organization-budget-and-finance/
+ * @copyright 2010 Speed School Student Council
+ * @author Jared Hatfield
+ * @package organization-budget-and-finance
+ * @version 1.0
+ */
+
+/// Get the raw search results for receipts
+function searchReceipts($searchString, $publicOnly){
+	global $database;
+	$query  = "SELECT `id`, `name`, `description`, `company`, `amount`, `lineitem`, `rdate`, `public` FROM `receipt` ";
+	$query .= "WHERE (`name` LIKE '%" . $searchString . "%' OR `description` LIKE '%" . $searchString . "%') ORDER BY `rdate` DESC;";
+	$result = $database->exec($query);
+	$val = array();
+	while($row = mysql_fetch_assoc($result)){
+		// If only public records are being accessed, only return those records
+		if(!($publicOnly && isReceiptPrivate($row['id']))){
+			$row['nav'] = getNavigationForLineItem($row['lineitem']);
+			$val[] = $row;
+		}
+	}
+	
+	return $val;
+}
+
+/// Get the raw search results for lineitems
+function searchLineItems($searchString, $publicOnly){
+	global $database;
+	$query  = "SELECT `id`, `name`, `description`, `parent`, `public` FROM lineitem ";
+	$query .= "WHERE (`name` LIKE '%" . $searchString . "%' OR `description` LIKE '%" . $searchString . "%') AND `id` != 1 ORDER BY `name`;";
+	$result = $database->exec($query);
+	$val = array();
+	while($row = mysql_fetch_assoc($result)){ 
+		// If only public records are being accessed, only return those records
+		if(!($publicOnly && isLineItemPrivate($row['id']))){
+			$row['nav'] = getNavigationForLineItem($row['parent']);
+			$val[] = $row;
+		}
+	}
+	
+	return $val;
+}
+
+?>
