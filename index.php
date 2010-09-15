900d88297eb991419364d05d98e0d19db34b5289
jjhatf02 <jjhatf02@90e9d07a-2c5a-11df-a14a-e9fb886a093d>
2010-09-15 12:44:13 +0000

    Limited the depth of the tree to 3 so that the queries used to access the information in the database can be optimized.

--- index.php
+++ index.php
@@ -141,7 +141,9 @@ else if($_GET['page'] == "budget"){
 	}
 	
 	$nav[] = Array("page" => "budget", "parms" => "", "text" => "Budget");
-	$nav = array_merge($nav, getNavigationForLineItem($parent));
+	$lnav = getNavigationForLineItem($parent);
+	$nav = array_merge($nav, $lnav);
+	$smarty->assign("treedepth", sizeof($lnav));
 	$smarty->assign("selectedTab","Budget");
 	$smarty->display('budget.tpl', $parent);
 }
@@ -161,7 +163,15 @@ else if($_GET['page'] == "lineitemAdd"){
 	
 	$smarty->assign("lineitemParent", getLineItem($parent));
 	$nav[] = Array("page" => "budget", "parms" => "", "text" => "Budget");
-	$nav = array_merge($nav, getNavigationForLineItem($parent));
+	$lnav = getNavigationForLineItem($parent);
+	$nav = array_merge($nav, $lnav);
+	
+	// We only allow the tree to grow to a depth of three.
+	if($lnav >= 3){
+		pageForbidden();
+	}
+	
+	$smarty->assign("treedepth", sizeof($lnav));
 	$nav[] = Array("page" => "lineitemAdd", "parms" => "lineid=".$parent, "text" => "Add Line Item");
 	$smarty->assign("selectedTab","Budget");
 	$smarty->display('lineitemAdd.tpl');
